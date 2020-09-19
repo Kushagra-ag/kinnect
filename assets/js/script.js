cdeb1dbc2b10a8469a0c06a0662f78abb6c62bcd
KshishAshish <17uec066@lnmiit.ac.in>
2020-09-19 21:44:15 +0530

    help center

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();   
+      });
 
     /*******************************Pagination ********************************************/
     $("div.holder").jPages({
